6ecc93924a7ef194e999eecc6c5f505012a3ba47
gabryelboeira <gabryel392@gmail.com>
2022-07-27 14:55:41 -0300

    Alterado classe que vai ser testada para receber a injecao de dependencia pelo construtor para facilitar os testes

--- testes-integracao/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ testes-integracao/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -4,13 +4,17 @@ import br.com.alura.leilao.model.Usuario;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.persistence.EntityManager;
+
 public class UsuarioDaoTest {
 
     private UsuarioDao dao;
 
+    private EntityManager em;
+
     @Test
     void testeBuscaDeUsuarioPeloUserName() {
-        this.dao = new UsuarioDao();
+        this.dao = new UsuarioDao(this.em);
         Usuario usuario = this.dao.buscarPorUsername("fulano");
 
         Assertions.assertNotNull(usuario);
